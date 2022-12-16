633bc1abb3fa889c6c3f5fd77e0599093375ed19
Matthew Buglass <matthewbuglass@gmail.com>
2022-12-16 15:17:01 -0600

    Added files for day 14

---
+++ 14/regolith_resevoir.py
@@ -0,0 +1,17 @@
+'''
+Problem Notes
+'''
+
+
+def run_tests(filename):
+    with open(filename, 'r') as f:
+        pass
+
+
+# testing
+run_tests('regolith_resevoir_test_input.txt')
+
+# Actual work
+# run_tests('regolith_resevoir_input.txt')
+
+
