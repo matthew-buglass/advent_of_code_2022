f8a1e0666b01db02763821fbe4d6a8a501ee3fff
Matthew Buglass <matthewbuglass@gmail.com>
2022-12-19 14:17:05 -0600

    Got the files able to be read in and print appropriately

--- 14/regolith_resevoir.py
+++ 14/regolith_resevoir.py
@@ -4,34 +4,52 @@ Problem Notes
 
 from pandas import DataFrame as df
 
+from const import flatten, color
 
-def get_positions_in_line_set(start: list[int, int], end: list[int, int]):
+
+def get_positions_in_line(start: tuple[int, int], end: tuple[int, int]):
     if start[0] == end[0]:
-        return {(start[0], c) for c in range(start[1], end[1])}
+        min_col = min(start[1], end[1])
+        max_col = max(start[1], end[1])
+        return [(start[0], c) for c in range(min_col, max_col + 1)]
     else:
-        return {(r, start[1]) for r in range(start[0], end[0])}
+        min_row = min(start[0], end[0])
+        max_row = max(start[0], end[0])
+        return [(r, start[1]) for r in range(min_row, max_row + 1)]
 
 
 class RockFormation:
     def __init__(self, puzzle_input: str):
-        self.locations = set()
-        self.min_x = 100000000
-        self.max_x = 0
-        self.max_y = 0
+        self.locations = []
+        self.min_col = 100000000
+        self.max_col = 0
+        self.max_row = 0
 
         traces: list[str] = [location for location in puzzle_input.split(' -> ')]
 
         for i in range(len(traces)-1):
-            start, end = [int(l) for l in traces[i]], [int(l) for l in traces[i+1]]
-            self.locations.union(get_positions_in_line_set(start, end))
+            start = [int(co_ord) for co_ord in traces[i].split(',')]
+            end = [int(co_ord) for co_ord in traces[i+1].split(',')]
+            self.locations += get_positions_in_line(
+                (start[0], start[1]),
+                (end[0], end[1])
+            )
+
+            self.min_col = min(self.min_col, start[0], end[0])
+            self.max_col = max(self.max_col, start[0], end[0])
+            self.max_row = max(self.max_row, start[1], end[1])
+
+    def get_normalized_coordinates(self, min_col):
+        return [(r, c - min_col) for c, r in self.locations]
 
 
 class RockFace:
     def __init__(self, puzzle_lines: list[str], sand_start: tuple[int, int]):
         self.rock_formations = [RockFormation(s) for s in puzzle_lines]
-        self.min_x = min([r.min_x] for r in self.rock_formations)
-        self.max_x = min([r.max_x] for r in self.rock_formations)
-        self.max_y = min([r.max_y] for r in self.rock_formations)
+        self.min_col = min([r.min_col for r in self.rock_formations])
+        self.max_col = max([r.max_col for r in self.rock_formations])
+        self.max_row = max([r.max_row for r in self.rock_formations])
+        self.col_labels = [str(_) for _ in range(self.min_col, self.max_col + 1)]
 
         self.sand_start = sand_start
         self.sand_locations = {}
@@ -39,13 +57,39 @@ class RockFace:
     def __str__(self):
         rock_sym = '#'
         air_sym = '.'
+        sand_start_sym = '+'
+        and_sym = 'o'
 
+        normalized_rocks = flatten([form.get_normalized_coordinates(self.min_col) for form in self.rock_formations])
+        norm_sand_start_row, norm_sand_start_col = self.sand_start[1], self.sand_start[0] - self.min_col
 
+        num_rows, num_cols = self.max_col - self.min_col + 1, self.max_row + 1
+
+        current_formation = [[air_sym for _ in range(num_cols)] for _ in range(num_rows)]
+
+        for x, y in normalized_rocks:
+            current_formation[x][y] = rock_sym
+
+        current_formation[norm_sand_start_row][norm_sand_start_col] = sand_start_sym
+
+        data_frame = df(current_formation)
+        return data_frame.to_string(header=self.col_labels)
 
 
 def run_tests(filename):
     with open(filename, 'r') as f:
-        pass
+        rock_wall = RockFace(
+            puzzle_lines=[l.strip() for l in f.readlines()],
+            sand_start=(500, 0)
+        )
+
+    print(rock_wall)
+
+    # pt 1.
+
+
+
+    # pt. 2
 
 
 # testing
--- const.py
+++ const.py
@@ -9,3 +9,8 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
+
+
+def flatten(l: list):
+   return [item for sublist in l for item in sublist]
+
